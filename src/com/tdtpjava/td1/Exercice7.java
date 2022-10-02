e96b034ddaa449a14dd06925b3204e0bfc7a1754
mehdiaitsaid <aitsaid.mahdi@gmail.com>
2022-10-02 14:59:42 +0100

    TD1

---
+++ src/com/tdtpjava/td1/Exercice7.java
@@ -0,0 +1,20 @@
+package com.tdtpjava.td1;
+
+import java.util.Scanner;
+
+public class Exercice7 {
+    public static void main(String[] args) {
+        int  n;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Entrez le premier entier :");
+        n = scanner.nextInt();
+
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
